4ecca819417df4bee3623c7bc08c29ef60571392
Haley Parker <90306479@ep-student.org>
2022-03-01 13:33:18 -0600

    first commit

--- Wordle/ViewController.swift
+++ Wordle/ViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var boxOne: UITextField!
+    
+    @IBOutlet weak var boxTwo: UITextField!
+    
+    @IBOutlet weak var boxThree: UITextField!
+    
+    @IBOutlet weak var boxFour: UITextField!
+    
+    @IBOutlet weak var boxFive: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        boxOne.delegate = self
+        boxTwo.delegate = self
+        boxThree.delegate = self
+        boxFour.delegate = self
+        boxFive.delegate = self
+        
+    }
+    
+    let allowedCharacters = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").inverted
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let currentText = textField.text ?? ""
+        
+        guard let stringRange = Range(range, in: currentText) else {return false}
+        
+        let updateText = currentText.replacingCharacters(in: stringRange, with: string)
+        
+        return updateText.count < 2
+        
     }
 
 
